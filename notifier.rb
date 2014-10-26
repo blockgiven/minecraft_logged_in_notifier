1547f2f73608cd725c8edc934e4b64f27596d171
block_given? <block_given@outlook.com>
2014-10-26 17:55:58 +0900

    ensure player count is integer

--- notifier.rb
+++ notifier.rb
@@ -9,7 +9,7 @@ prev_player_count = redis.get(count_key) || 0
 status_response = Takumi::ServerListPing.ping(ENV['MINECRAFT_SERVER_ADDRESS'])
 current_player_count = status_response.info['players']['online']
 
-if current_player_count > prev_player_count
+if current_player_count.to_i > prev_player_count.to_i
   idobata_hook_client = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
   idobata_hook_client.send("@all 誰かがマイクラはじめたみたい")
 end
