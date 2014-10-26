bd9ac75efca8453cef707e5402fe08bce0000bb6
block_given? <block_given@outlook.com>
2014-10-26 17:10:42 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org/'
+
+ruby '2.1.3'
+
+gem 'idobata_hook'
+gem 'redis'
+gem 'takumi-server_list_ping'
---
+++ notifier.rb
@@ -0,0 +1,17 @@
+require 'redis'
+require 'takumi/server_list_ping'
+
+count_key = 'online_player_count'
+redis = Redis.new(url: ENV['REDISTOGO_URL'])
+
+prev_player_count = redis.get(count_key) || 0
+
+status_response = Takumi::ServerListPing.ping(ENV['MINECRAFT_SERVER_ADDRESS'])
+current_player_count = status_response.info['players']['online']
+
+if current_player_count > prev_player_count
+  idobata_hook_client = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
+  idobata_hook_client.send("@all 誰かがマイクラはじめたみたい")
+end
+
+redis.set(count_key, current_player_count)
