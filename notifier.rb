8afbea2e9c34e244daec99036f49a76ceafcf0cd
block_given? <block_given@outlook.com>
2014-10-29 00:34:56 +0900

    loop!

--- notifier.rb
+++ notifier.rb
@@ -5,17 +5,23 @@ require 'takumi/server_list_ping'
 count_key = 'online_player_count'
 redis = Redis.new(url: ENV['REDISTOGO_URL'])
 
-prev_player_count = redis.get(count_key) || 0
+loop do
+  prev_player_count = redis.get(count_key) || 0
 
-status_response = Takumi::ServerListPing.ping(ENV['MINECRAFT_SERVER_ADDRESS'])
-current_player_count = status_response.info['players']['online']
-if current_player_count.to_i > prev_player_count.to_i
-  player_names = status_response['players']['sample'].map {|player|
-    player['name']
-  }.join(', ')
+  status_response = Takumi::ServerListPing.ping(ENV['MINECRAFT_SERVER_ADDRESS'])
 
-  idobata_hook_client = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
-  idobata_hook_client.send("@all #{player_names}がマイクラはじめたみたい")
+  current_player_count = status_response.info['players']['online']
+
+  if current_player_count.to_i > prev_player_count.to_i
+    player_names = status_response['players']['sample'].map {|player|
+      player['name']
+    }.join(', ')
+
+    idobata_hook_client = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
+    idobata_hook_client.send("@all #{player_names}がマイクラはじめたみたい")
+  end
+
+  redis.set(count_key, current_player_count)
+
+  sleep 60
 end
-
-redis.set(count_key, current_player_count)
