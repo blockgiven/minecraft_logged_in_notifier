f7fb57024c37b1f9fbad60256aa21dcd95676bc0
block_given? <block_given@outlook.com>
2014-10-29 00:20:07 +0900

    player name

--- notifier.rb
+++ notifier.rb
@@ -9,10 +9,13 @@ prev_player_count = redis.get(count_key) || 0
 
 status_response = Takumi::ServerListPing.ping(ENV['MINECRAFT_SERVER_ADDRESS'])
 current_player_count = status_response.info['players']['online']
-
 if current_player_count.to_i > prev_player_count.to_i
+  player_names = status_response['players']['sample'].map {|player|
+    player['name']
+  }.join(', ')
+
   idobata_hook_client = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
-  idobata_hook_client.send("@all 誰かがマイクラはじめたみたい")
+  idobata_hook_client.send("@all #{player_names}がマイクラはじめたみたい")
 end
 
 redis.set(count_key, current_player_count)
